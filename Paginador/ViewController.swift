7aedad3eacbcc5218594649d309ead041902c68a
Erwin Luz Leon <eluz@bancoazteca.com>
2021-03-20 14:16:56 -0600

    se limpia proyecto

--- Paginador/ViewController.swift
+++ Paginador/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Paginador
-//
-//  Created by edeluz on 20/03/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
